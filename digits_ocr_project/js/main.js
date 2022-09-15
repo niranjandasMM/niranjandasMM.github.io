cbdac14bc82c915cca3a377b470f71d92a0a38d9
Niranjanadas M <85221814+niranjandasMM@users.noreply.github.com>
2022-09-15 12:06:10 +0530

    updated main.js
    Added cross-origin keyword to avoid the cross-origin error

--- digits_ocr_project/js/main.js
+++ digits_ocr_project/js/main.js
@@ -1,4 +1,4 @@
-console.log("Hai javscript here ")
+console.log("Hai javscript here, added cross-origin ")
 var loadFile = function(event) {
     var image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0]);
